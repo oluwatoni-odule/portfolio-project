49cc92046368a5e169112d45d1e5c1564fd26e0e
oluwatoni-odule <oluwatoniodule@gmail.com>
2024-03-24 20:27:31 +0100

    Scroll reveal update

--- script.js
+++ script.js
@@ -253,10 +253,7 @@ const sr = ScrollReveal({
   reset: true
 });
 
-sr.reveal(`.home__title, .popular__container, .subscribe__container, .footer__container`);
-sr.reveal(`.home__description, .footer__info`, {delay: 500});
-sr.reveal(`.home__search`, {delay: 600});
-sr.reveal(`.home__images`, {delay: 800, origin: 'bottom'});
-sr.reveal(`.logos__img`, {interval: 100});
-sr.reveal(`.value__images, .contact__content`, {origin: 'left'});
-sr.reveal(`.value__content, .contact__images`, {origin: 'right'});
+
+sr.reveal(`.about-content, .project, .skill`, {delay: 200, origin: 'bottom'});
+sr.reveal(`.skills-content`, {interval: 100});
+sr.reveal(`.image-container`, {origin: 'right'});
